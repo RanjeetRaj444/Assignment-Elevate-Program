db9bee82c724924695d48c94ec4001ce99eb16d3
RanjeetRaj444 <ranjeetraj44666@gmail.com>
2025-06-09 19:55:04 +0530

    Longest Consecutive Sequence

--- DSA-day-7/Longest-Consecutive-Sequence.js
+++ DSA-day-7/Longest-Consecutive-Sequence.js
@@ -4,3 +4,27 @@
 // Input: [100, 4, 200, 1, 3, 2]
 
 // Output: 4 → because the sequence [1, 2, 3, 4] is the longest
+
+let n = nums.length;
+
+if (n === 0) {
+  return 0;
+}
+
+nums.sort((a, b) => a - b);
+
+let count = 1;
+let maxCount = 0;
+
+for (let i = 1; i < n; i++) {
+  if (nums[i] !== nums[i - 1]) {
+    if (nums[i] === nums[i - 1] + 1) {
+      count++;
+    } else {
+      maxCount = Math.max(maxCount, count);
+      count = 1;
+    }
+  }
+}
+
+return Math.max(maxCount, count);
